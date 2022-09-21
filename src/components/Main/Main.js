bad1bf40efb38bed16f74d71a2b9f88f0af50828
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-20 17:46:11 -0700

    filters

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import useCountries from '../../hooks/useCountries';
 import Country from '../Country/Country';
+import Filter from '../Filter/Filter';
 
 export default function Main() {
   const { filterCountries, continent, setContinent } = useCountries(); 
   return (
     <div>
+      <h1>Flags!!!</h1>
+      <Filter continent={continent} setContinent={setContinent} />
       {filterCountries().map((name) => (
         <Country key={name.id} {...name} />
       ))}
